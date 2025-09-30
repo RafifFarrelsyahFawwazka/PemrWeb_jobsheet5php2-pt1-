f8ec56761e27d1c9c4d5e50d4cea32c5e2639476
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2025-09-30 20:09:52 +0700

    Add files via upload

--- fungsi.php
+++ fungsi.php
@@ -1,25 +1,4 @@
 <?php
-
-// Membuat fungsi
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam . ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br>";
-    echo "Senang berkenalan dengan Anda<br>";
-}
-
-// Memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-
-// Memanggil lagi tanpa mengisi parameter salam
-perkenalan($saya);
-
-echo "<hr>";
-
 // Membuat fungsi
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
