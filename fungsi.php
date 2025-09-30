87a9b2b7809b862339f5d9a0c7a564844f00c61f
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2025-09-30 19:43:16 +0700

    Add files via upload

--- fungsi.php
+++ fungsi.php
@@ -18,4 +18,23 @@ $ucapanSalam = "Selamat pagi";
 // Memanggil lagi tanpa mengisi parameter salam
 perkenalan($saya);
 
+echo "<hr>";
+
+// Membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br>";
+
+    // Memanggil fungsi lain
+    echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+
+// Memanggil fungsi perkenalan
+    perkenalan ("Elok");
 ?>
