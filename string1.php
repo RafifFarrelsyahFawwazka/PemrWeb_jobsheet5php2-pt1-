2ce037121204935f161ef68a49e40aa595ff503c
RafifFarrelsyahFawwazka <rafifcer123@gmail.com>
2025-09-30 20:37:24 +0700

    Add files via upload

---
+++ string1.php
@@ -0,0 +1,11 @@
+<?php
+
+$loremIpsum = "Ini adalah Teks";
+
+echo "<p>{$loremIpsum}</p>";
+echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
+echo "Panjang kata: " . str_word_count($loremIpsum) . "<br>";
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p>";
+
+?>
